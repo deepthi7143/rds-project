430f4d1f0d39ae28157f309e790121314939ee7d
deepthi7143 <150417779+deepthi7143@users.noreply.github.com>
2024-01-16 14:22:43 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,81 @@
+
+variable "main_allocated_storage"{ 
+type = number 
+default = "20" 
+} 
+ 
+variable "main_db_engine"{ 
+type = string 
+default = "MySQL" 
+} 
+ 
+variable "main_db_instance_class"{ 
+type = string 
+default = "db.t2.micro" 
+ 
+} 
+ 
+variable "main_db_username" { 
+type = string   
+default = "admin" 
+} 
+ 
+variable "parameter_group_name" { 
+    type = string  
+default = "" 
+} 
+ 
+variable "password" { 
+type = string  
+default ="password"  
+} 
+ 
+variable "main_db_version" { 
+    type = string 
+  default = "8.0.35" 
+} 
+ 
+variable "replica_instance_class" { 
+    type = string 
+    default = "db.t2.micro" 
+   
+} 
+ 
+variable "replica_count" { 
+  type = number 
+  default = 1
+} 
+ 
+variable "rds_replica_azs" { 
+  type = list(string) 
+  default = ["ap-south-1"] 
+} 
+variable "sg_vpc_id" { 
+    type = string 
+    default = "vpc-02f97ab5ea86f7d26" 
+   
+} 
+variable "sg_ingress_from_port" { 
+    type = number 
+    default = 3306 
+   
+} 
+variable "sg_ingress_to_port" { 
+    type = number 
+    default = 3306 
+   
+} 
+ 
+variable "cidr"{ 
+type = list(string) 
+default = [ "0.0.0.0/0" ] 
+} 
+ 
+variable "sg_protocol" { 
+    type = string 
+    default = "tcp" 
+} 
+variable "sg_name" { 
+  type = string 
+  default = "" 
+}
